6392d49c47b26aae73182e4151154355cc4b0ea0
Gabriele <mattiazzi.gabriele@gmail.com>
2026-08-05 11:10:57 +0200

    next step

---
+++ include/noise_filter.h
@@ -0,0 +1,11 @@
+#ifndef MTTI2T_INCLUDE_NOISE_FILTER_H_
+#define MTTI2T_INCLUDE_NOISE_FILTER_H_
+
+namespace mtti2t {
+  namespace noise_filters {
+    // dilatation & erosion
+    // opening & closing
+  };
+}
+
+#endif
